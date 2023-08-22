b8e904b00e44ae2cc325ca7ad8c201443d53c831
Aleksey Macegora <a.macegora@emcd.io>
2023-08-23 00:27:16 +0600

    ios ui

--- ios/Classes/SwiftIdenfySdkFlutterPlugin.swift
+++ ios/Classes/SwiftIdenfySdkFlutterPlugin.swift
@@ -19,20 +19,21 @@ public class SwiftIdenfySdkFlutterPlugin: NSObject, FlutterPlugin {
 
 
                 //Changing common iDenfy colors
-                IdenfyCommonColors.idenfyMainColorV2 = UIColor.green
-                IdenfyCommonColors.idenfyMainDarkerColorV2 = UIColor.green
-                IdenfyCommonColors.idenfySecondColorV2 = UIColor.black
-                IdenfyCommonColors.idenfyBackgroundColorV2 = UIColor.white
+                // IdenfyCommonColors.idenfyMainColorV2 = UIColor.green
+                // IdenfyCommonColors.idenfyMainDarkerColorV2 = UIColor.green
+                // IdenfyCommonColors.idenfySecondColorV2 = UIColor.black
+                // IdenfyCommonColors.idenfyBackgroundColorV2 = UIColor.white
 
-                //Customizing Tooblar
-                IdenfyToolbarUISettingsV2.idenfyDefaultToolbarLogoIconTintColor = UIColor.blue
-                IdenfyToolbarUISettingsV2.idenfyDefaultToolbarBackIconTintColor = UIColor.blue
-                IdenfyToolbarUISettingsV2.idenfyLanguageSelectionToolbarLanguageSelectionIconTintColor = UIColor.yellow
-                IdenfyToolbarUISettingsV2.idenfyLanguageSelectionToolbarCloseIconTintColor = UIColor.blue
-                IdenfyToolbarUISettingsV2.idenfyCameraPreviewSessionToolbarBackIconTintColor = UIColor.white
+                // //Customizing Tooblar
+                // IdenfyToolbarUISettingsV2.idenfyDefaultToolbarLogoIconTintColor = UIColor.blue
+                // IdenfyToolbarUISettingsV2.idenfyDefaultToolbarBackIconTintColor = UIColor.blue
+                // IdenfyToolbarUISettingsV2.idenfyLanguageSelectionToolbarLanguageSelectionIconTintColor = UIColor.yellow
+                // IdenfyToolbarUISettingsV2.idenfyLanguageSelectionToolbarCloseIconTintColor = UIColor.blue
+                // IdenfyToolbarUISettingsV2.idenfyCameraPreviewSessionToolbarBackIconTintColor = UIColor.white
 
                 //Configuring IdenfyUISettings
                 let idenfyUISettingsV2 = IdenfyUIBuilderV2()
+                    .withIdenfyToolbarHidden()
                     .build()
 
                 let idenfySettingsV2 = IdenfyBuilderV2()
